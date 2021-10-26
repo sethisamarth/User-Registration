0c20e54b019e8a397f94b7c0969db21bb2745edf
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-26 09:22:35 +0530

    First Name

---
+++ UserRegistration/UserRegistration/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+   public class Pattern
+    {
+        public static string Regex_FirstName = "^[A-D][a-zA-Z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, Regex_FirstName);
+        }
+    }
+}
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,6 +7,14 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration!!!");
+            while (true)
+            {
+                Pattern p = new Pattern();
+                Console.WriteLine("Enter your FirstName");
+                var check = p.ValidateFirstName(Console.ReadLine());
+                Console.WriteLine(check);
+
+            }
         }
     }
 }

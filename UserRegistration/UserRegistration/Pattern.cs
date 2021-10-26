adfe87956e7a1df5791659e43f6ad61177e7da90
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-26 09:39:23 +0530

    Last Name

---
+++ UserRegistration/UserRegistration/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Pattern
+    {
+        public static string Regex_FirstName = "^[A-D][a-zA-Z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, Regex_LastName);
+        }
+        public static string Regex_LastName = "^[A-D][a-zA-Z]{2,}$";
+        public bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, Regex_LastName);
+        }
+    }
+}
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,6 +7,14 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration!!!");
+            while (true)
+            {
+                Pattern p = new Pattern();
+                Console.WriteLine("Enter your LastName");
+                var check = p.ValidateLastName(Console.ReadLine());
+                Console.WriteLine(check);
+
+            }
         }
     }
 }

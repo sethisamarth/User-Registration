eac77398a0cf0f64555de30237470ec2a02d088f
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-30 16:11:21 +0530

    Validating by testing

---
+++ UserRegistration/TestProject1/UnitTest1.cs
@@ -0,0 +1,102 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistration;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {//validation for first Name
+        [TestMethod]
+        [DataRow("Samath", "Samath")]
+        [DataRow("Sa", "Sa")]
+        [DataRow("s", null)]
+        [DataRow("Sama05", null)]
+
+        public void ValidateFirstname(string a, string expected)
+        {
+
+            var actual = RegexSample.ValidatingFirstName(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validating for Last Name
+        [TestMethod]
+        [DataRow("Sethi", "Sethi")]
+        [DataRow("Sethi", null)]
+        [DataRow("SethiSamarth", "SethiSamarth")]
+        [DataRow("Samarth05", null)]
+        public void ValidateUserLastname(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingLastName(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validation for Email
+        [TestMethod]
+        [DataRow("abc@gmail.com", "abc@gmail.com")]
+        [DataRow("abc-100@yahoo.com", "abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com", "abc.100@yahoo.com")]
+        [DataRow("abc111@yahoo.com", "abc111@yahoo.com")]
+        [DataRow("abc111@abc.com", "abc111@abc.com")]
+        [DataRow("abc-100@abc.net", "abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au", "abc.100@abc.com.au")]
+        [DataRow("abc@1.com", "abc@1.com")]
+        [DataRow("abc@gmail.com.com", "abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com", "abc+100@gmail.com")]
+        [DataRow("abc", null)]
+        [DataRow("abc@.com.my", null)]
+        [DataRow("abc123@.com", null)]
+        [DataRow("abc123@.com.com", null)]
+        [DataRow("abc()*@gmail.com", null)]
+        [DataRow(".abc@abc.com", null)]
+        [DataRow("abc@%*.com", null)]
+        [DataRow("abc..2002@gmail.com", null)]
+        [DataRow("abc.@gmail.com", null)]
+        [DataRow("abc@abc@gmail.com", null)]
+        [DataRow("abc@gmail.com.1a", null)]
+        [DataRow("abc@gmail.com.aa.au", null)]
+
+        public void ValidateUserEmail(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingEmailId(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Validation for Phone Number
+        [TestMethod]
+        [DataRow("1 1000987267", "1 1000987267")]
+        [DataRow("91 9842905050", "91 9842905050")]
+        [DataRow("100 9842905050", "100 9842905050")]
+        [DataRow("919842905050", null)]
+        [DataRow("919842905", null)]
+        [DataRow("91 984290", null)]
+        [DataRow("91 984290505000000", null)]
+        public void ValidateUserPhoneNumber(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingPhoneNum(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        ////Validation for Password
+        [TestMethod]
+        [DataRow("Sam@123", "Sam@123")]
+        [DataRow("Sam#123", "Sam#123")]
+        [DataRow("Sam@1S ", "Sam@1S")]
+        [DataRow("@sam129", "@Sam129")]
+        [DataRow("Sa-m123", "Sa-m123")]
+        [DataRow("sam123)@1234", null)]
+        [DataRow("Sam@lini", null)]
+        [DataRow("sam123", null)]
+        [DataRow("sam@123", null)]
+        [DataRow("@sam#12sam", null)]
+        [DataRow(")sam12", null)]
+        [DataRow(")sam12", null)]
+        [DataRow("sa-m@12S", null)]
+        [DataRow("sam@s", null)]
+
+        public void ValidateUserPassword(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingPassWord(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
